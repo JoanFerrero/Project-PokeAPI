a912200fbe5b8867b9ccf7fe24fdc04e8609c8f0
unknown <joan.ferrero.montiel@gmail.com>
2023-01-26 10:55:36 +0100

    add Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,33 @@
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from './logo.png';
+
+const Header = () => {
+    return (
+      <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="home">
+          <img 
+            src={logo} 
+            width="30"
+            height="30"
+            alt="logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" >
+          <Nav className="me-auto">
+            <Nav.Link href="home">Home</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="heart"><FaHeart /></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    )
+}
+
+export default Header;
